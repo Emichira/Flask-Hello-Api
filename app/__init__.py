531acd3dc85539bfdf89d3a206fde45e8d81cd39
Emichira <emmanuelmichira@gmail.com>
2018-03-26 20:11:02 +0300

    Install Fask-API and initialize it

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+from flask_api import FlaskAPI
+
+# local import
+from instance.config import app_config
+
+def create_app(config_name):
+    #create instance of flaskapi
+    app=FlaskAPI(__name__,instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+
+    return app
