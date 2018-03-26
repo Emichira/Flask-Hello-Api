45dccfcfe29041d7d55a308180075f04a597e5d6
Emichira <emmanuelmichira@gmail.com>
2018-03-26 20:13:44 +0300

    Modify config file

--- instance/config.py
+++ instance/config.py
@@ -7,7 +7,6 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
@@ -16,7 +15,6 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
     DEBUG = True
 
 class StagingConfig(Config):
