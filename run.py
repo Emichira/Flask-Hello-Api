a487a9c0bee93fecf06a47ebb8216a8d9d96e421
Emichira <emmanuelmichira@gmail.com>
2018-03-28 03:53:08 +0300

    Add url rule to run server

--- run.py
+++ run.py
@@ -1,12 +1,25 @@
 
-import os
-from app import create_app
+from app import User, create_app, Book
+from app import reset_password, login, register, logout, add_book
 
-config_name=os.getenv('APP_SETTINGS') # config_name = "development"
+create_app.add_url_rule(
+    '/api/v1/auth/register', view_func=register.as_view(
+        'register'), methods=['GET', 'POST'])
+create_app.add_url_rule(
+    '/api/v1/auth/login', view_func=login.as_view(
+        'login'), methods=['GET', 'POST'])
+create_app.add_url_rule(
+    '/api/v1/books', view_func=add_book.as_view(
+        'books'), methods=['GET', 'POST'])
+create_app.add_url_rule(
+    '/api/v1/auth/logout', view_func=logout.as_view(
+        'logout'), methods=['POST'])
 
-app=create_app(config_name)
+create_app.add_url_rule(
+    '/api/v1/auth/reset-password', view_func=reset_password.as_view(
+        'reset-password '), methods=['POST'])
 
 if __name__ == '__main__':
     
-    app.run()
+    create_app.run()
 
