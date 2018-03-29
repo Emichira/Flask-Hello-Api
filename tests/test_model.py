64cfa386344045176dea362e7dc1c3fa04ed551f
Emichira <emmanuelmichira@gmail.com>
2018-03-29 17:29:28 +0300

    Change typo error for initializing app

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,14 @@
 import unittest, app
 from app.models import Book, User
 import json
-from app import create_app
+from run import create_app
 from flask import jsonify
 import run
 
 class BookCreation(unittest.TestCase):
     
     def setUp(self):
-        self.client = run.create-app.test_client
+        self.client = run.create_app.test_client
         self.book = {"ISBN": "00001", "Title": "MacBeth", "Author": "Shakespear", "Date-Published": "12/10/2018","category": "Good Reads"}
 
     def tearDown(self):
